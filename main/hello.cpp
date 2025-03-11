a6f440c48a44ff48b1564f53e256b0b160ac9d3a
Sarang m <sarangmInfy@gmail.com>
2025-03-11 22:49:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World sfasss\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this sajnfala  \n";
   return 0;
